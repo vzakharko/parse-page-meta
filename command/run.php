25fd8d502593ece59d0d7c64425b03f7d6fa3bb3
Vasilii Zakharko <vzakharko@aristeksystems.com>
2018-04-06 15:17:49 +0300

    first commit

---
+++ command/run.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: developer
+ * Date: 4/3/18
+ * Time: 4:53 PM
+ */
+require_once __DIR__.'/../vendor/autoload.php';
---
+++ src/parse.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: developer
+ * Date: 4/3/18
+ * Time: 4:59 PM
+ */
